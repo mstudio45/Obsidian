9c9f980118147cc50e339ec42978ec028d243988
mhhh <mmmhlecacamdrr@proton.me>
2026-01-01 14:27:22 +0100

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -669,6 +669,10 @@ Tabs.Key:AddKeyBox(function(ReceivedKey)
 	})
 end)
 
+-- DraggableLabel
+
+Library:AddDraggableLabel("This is a Draggable Label")
+
 -- UI Settings
 local MenuGroup = Tabs["UI Settings"]:AddLeftGroupbox("Menu", "wrench")
 
